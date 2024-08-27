7272bd2d80c99f46b362d057659863078bcf83ab
Sparsh kandpal <ksparsh443@gmail.com>
2024-08-27 08:27:45 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,23 @@
+// server.js
+const WebSocket = require('ws');
+const server = new WebSocket.Server({ port: 8080 });
+
+server.on('connection', ws => {
+  console.log('Client connected');
+
+  ws.on('message', message => {
+    console.log(`Received message: ${message}`);
+    // Broadcast the message to all connected clients
+    server.clients.forEach(client => {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
+    });
+  });
+
+  ws.on('close', () => {
+    console.log('Client disconnected');
+  });
+});
+
+console.log('WebSocket server is running on ws://localhost:8080');
